bd4cf866d3f51a0b21ce6a0d0b676bdc1e8a8df0
yull <gmldnd93@gmail.com>
2024-10-08 17:30:56 +0900

    project

---
+++ dto/OpenDTO.java
@@ -0,0 +1,29 @@
+package dto;
+
+public class OpenDTO {
+	String name = null;
+	String title = null;
+	String ion = null;
+	
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name; 
+	}
+	public String getIon() {
+		return ion;
+	}
+	public void setIon(String ion) {
+		this.ion = ion;
+	}
+	
+}
---
+++ main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import service.OpenService;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("아이디어 공모전 프로그램 개발");
+		OpenService ar = new OpenService(); 
+	}
+}
